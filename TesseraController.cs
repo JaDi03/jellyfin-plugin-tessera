428818f31d5ffa5bcedeb0b519f5387177727fe3
Test User <test@test.com>
2026-07-27 13:47:19 -0600

    feat: standardize paywall architecture via proxy relay

--- TesseraController.cs
+++ TesseraController.cs
@@ -29,5 +29,39 @@ namespace Jellyfin.Plugin.Tessera
                 }
             }
         }
+
+        [HttpGet("assets/{filename}")]
+        public async System.Threading.Tasks.Task<IActionResult> GetAsset(string filename)
+        {
+            var serverUrl = Plugin.Instance?.Configuration?.TesseraServerUrl ?? "http://localhost:7878";
+            serverUrl = serverUrl.TrimEnd('/');
+            string url = $"{serverUrl}/peertube-assets/{filename}";
+
+            using (System.Net.Http.HttpClient client = new System.Net.Http.HttpClient())
+            {
+                try
+                {
+                    System.Net.Http.HttpResponseMessage response = await client.GetAsync(url);
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        return StatusCode((int)response.StatusCode, "Failed to fetch asset from sidecar");
+                    }
+
+                    string contentType = "application/octet-stream";
+                    if (filename.EndsWith(".js")) contentType = "application/javascript; charset=utf-8";
+                    else if (filename.EndsWith(".css")) contentType = "text/css; charset=utf-8";
+                    else if (filename.EndsWith(".svg")) contentType = "image/svg+xml";
+                    else if (filename.EndsWith(".png")) contentType = "image/png";
+                    else if (filename.EndsWith(".ico")) contentType = "image/x-icon";
+
+                    var stream = await response.Content.ReadAsStreamAsync();
+                    return File(stream, contentType);
+                }
+                catch (System.Exception ex)
+                {
+                    return StatusCode(502, $"Could not reach Tessera sidecar: {ex.Message}");
+                }
+            }
+        }
     }
 }
